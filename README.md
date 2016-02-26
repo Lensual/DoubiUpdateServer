aa8231b58bf73d5b2e6ef3c0d506556b3325919a
Lensual <lenvisual@gmail.com>
2016-02-26 21:18:52 +0800

    0.2

---
+++ README.md
@@ -0,0 +1,22 @@
+# DoubiUpdateServer
+从DoubiLauncher项目0.1.0.0版本分离
+
+用于作为软件更新的服务端、更新源
+
+需要web服务和PHP支持
+
+# webroot文件夹
+web站点根目录
+
+index.php 可更改为其他名字
+
+#PS
+有的东西偷懒先不写了w
+
+会慢慢写上去的（¯﹃¯）
+
+作者主页 http://lensual.dreamerstudio.net/blog
+
+项目主页 http://www.dreamerstudio.net/doubiupdateserver
+
+GitHub http://github.com/Lensual/DoubiUpdateServer
---
+++ webroot/update/DoubiLauncher/index.php
@@ -0,0 +1,69 @@
+<?php
+	//服务器设置
+	$ServerVersion = 0.2; //服务器处理程序版本
+	
+	$LastestVersion_Release = '0.1.0.0'; //最后的Release版本
+	$LastestVersion_Beta = '0.1.0.0'; //最后的Beta版本
+	$LastestVersion_Debug = '0.2.1.0'; //最后的Debug版本
+	
+	$LastestBuild_Release = '0'; //最后的Release Build
+	$LastestBuild_Beta = '0'; //最后的Beta Build
+	$LastestBuild_Debug = '1'; //最后的Debug Build
+	
+	//处理请求
+	switch (@$_GET['Command']){
+		case 'GetServerVersion': //查询更新服务器版本
+			echo $ServerVersion;
+			break;
+		case 'GetLastestVersion': //查询最后客户端版本
+			switch (@$_GET['Version']){
+				case 'Release':
+					echo $LastestVersion_Release;
+					break;
+				case 'Beta':
+					echo $LastestVersion_Beta;
+					break;
+				case 'Debug':
+					echo $LastestVersion_Debug;
+					break;
+				default:
+					echo 'Unknown Parameter';
+			}
+			break;
+		case 'Download': //下载文件
+			//打开文件
+			$file_path = "./Download/" . @$_GET['Version'] . "/" . @$_GET['Version'] . ".exe";
+			$file_size = filesize ( "./Download/" . @$_GET['Version'] . "/" . @$_GET['Version'] . ".exe" );
+			$file = fopen ( $file_path , "r" );
+			//输入文件标签
+			Header( "Content-type: application/octet-stream" );
+			Header( "Accept-Ranges: bytes" );
+			Header( "Accept-Length: " . $file_size );
+			Header( "Content-Disposition: attachment; filename=" . @$_GET['Version'] . ".exe" );
+			//输出文件内容
+			//读取文件内容并直接输出到浏览器
+			echo fread( $file, $file_size );
+			fclose( $file );
+			exit();
+			break;
+		case 'WhatShouldIdo': //获取更新脚本
+			if (@$_GET['Build']!=$LastestBuild_Debug){
+				echo 'ask(发现新版本V0.2.1.0是否更新？,end);';
+				echo 'mkdir(.\doubitemp);';
+				echo 'download(http://update.dreamerstudio.net/update/DoubiLauncher/Download/0.2.0.1/0.2.0.1.exe,.\doubitemp\DoubiLauncher V0.2.0.1.exe);';
+				echo 'move(.\doubitemp\DoubiLauncher V0.2.0.1.exe,.\DoubiLauncher V0.2.0.1.exe);';
+				echo 'delete(.\doubitemp);';
+				echo 'start(.\DoubiLauncher V0.2.0.1.exe,,false);';
+				echo 'deleteme();';
+				echo 'sign(end);';
+			}
+			break;
+			
+		default:
+			echo 'Unknown Command';
+	}
+	
+	//生成脚本语句函数
+	//（暂时假装有内容（¯﹃¯））
+
+?>
